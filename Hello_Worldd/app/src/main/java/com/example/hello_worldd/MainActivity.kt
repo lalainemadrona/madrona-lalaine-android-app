57127707bb64dd5b6165ac5c5b2336dcb3522230
aDMin <lalainemina.madrona@bicol-u.edu.ph>
2021-04-12 23:28:14 +0800

    first commit

---
+++ Hello_Worldd/app/src/main/java/com/example/hello_worldd/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.hello_worldd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /* hello this is lalaine haha */
+    }
+}
